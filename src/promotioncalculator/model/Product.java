2a3374d85c6dcbc7ed35d1e1747496c98f642644
Mohit <mohit0maheshwari@gmail.com>
2021-12-12 20:43:39 +0000

    Remove the Unwanted Import

--- src/promotioncalculator/model/Product.java
+++ src/promotioncalculator/model/Product.java
@@ -1,33 +1,9 @@
 package promotioncalculator.model;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Map;
 
 public abstract class Product {
-
-    private String skuIDs;
-
-    private BigDecimal price;
-
-
     public abstract BigDecimal getTotalPrice(Map<String, Integer> productOrders);
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getSkuIDs() {
-        return skuIDs;
-    }
-
-    public void setSkuIDs(String skuIDs) {
-        this.skuIDs = skuIDs;
-    }
-
 
 }
